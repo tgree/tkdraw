acf4e0b1b9a7e07780af0c6137e278370e556074
Terry Greeniaus <terrygreeniaus@gmail.com>
2022-04-10 00:18:34 -0700

    tkdraw/workspace.py: Add padding to the grid so that all points are contained inside the canvas.

--- tkdraw/workspace.py
+++ tkdraw/workspace.py
@@ -13,6 +13,7 @@ TOOLS_HEIGHT = 400
 DRAW_WIDTH   = WINDOW_WIDTH - TOOLS_WIDTH
 DRAW_HEIGHT  = WINDOW_HEIGHT
 GRID_SPACING = 10
+GRID_PAD     = (GRID_SPACING // 2)
 
 
 def clamp(l, v, r):
@@ -37,8 +38,12 @@ class DrawCanvas(Canvas):
         self.v_rects       = []
         self.width_points  = None
         self.height_points = None
-        self.border_rect   = self.add_rectangle(0, 0, 0, 0, outline='',
+        self.content_rect  = self.add_rectangle(0, 0, 0, 0, outline='',
                                                 fill='black')
+        self.h_pad         = self.add_rectangle(0, 0, 0, 0, fill='white',
+                                                outline='')
+        self.v_pad         = self.add_rectangle(0, 0, 0, 0, fill='white',
+                                                outline='')
         self.register_handler('<Configure>', self._handle_config_change)
 
     def _handle_config_change(self, e):
@@ -55,23 +60,28 @@ class DrawCanvas(Canvas):
         point.  This is O(M + N) and performance is snappy, at least on my
         M1 MacBook Pro.
         '''
-        self.border_rect.resize(GRID_SPACING, GRID_SPACING, e.width - 1,
-                                e.height - 1)
-        self.width_points  = e.width // GRID_SPACING
-        self.height_points = e.height // GRID_SPACING
+        self.content_rect.resize(GRID_PAD - 1, GRID_PAD - 1, e.width - 1,
+                                 e.height - 1)
+        self.width_points  = (e.width - GRID_PAD - 1) // GRID_SPACING
+        self.height_points = (e.height - GRID_PAD - 1) // GRID_SPACING
 
+        self.h_pad.resize(0, 0, e.width, GRID_PAD)
         for y, r in enumerate(self.h_rects):
-            r.resize(0, y * GRID_SPACING + 1, e.width, GRID_SPACING - 1)
+            r.resize(0, y * GRID_SPACING + GRID_PAD + 1, e.width,
+                     GRID_SPACING - 1)
         for y in range(len(self.h_rects), e.height // GRID_SPACING + 1):
-            r = self.add_rectangle(0, y * GRID_SPACING + 1, e.width,
+            r = self.add_rectangle(0, y * GRID_SPACING + GRID_PAD + 1, e.width,
                                    GRID_SPACING - 1, fill='white', outline='')
             self.h_rects.append(r)
 
+        self.v_pad.resize(0, 0, GRID_PAD, e.height)
         for x, r in enumerate(self.v_rects):
-            r.resize(x * GRID_SPACING + 1, 0, GRID_SPACING - 1, e.height)
+            r.resize(x * GRID_SPACING + GRID_PAD + 1, 0, GRID_SPACING - 1,
+                     e.height)
         for x in range(len(self.v_rects), e.width // GRID_SPACING + 1):
-            r = self.add_rectangle(x * GRID_SPACING + 1, 0, GRID_SPACING - 1,
-                                   e.height, fill='white', outline='')
+            r = self.add_rectangle(x * GRID_SPACING + GRID_PAD + 1, 0,
+                                   GRID_SPACING - 1, e.height, fill='white',
+                                   outline='')
             self.v_rects.append(r)
 
 
@@ -103,8 +113,10 @@ class Workspace(TKBase):
         if e.widget != self.canvas._canvas:
             return
 
-        x = clamp(0, round(x / GRID_SPACING), self.canvas.width_points)
-        y = clamp(0, round(y / GRID_SPACING), self.canvas.height_points)
+        x = clamp(0, round((x - GRID_PAD) / GRID_SPACING),
+                  self.canvas.width_points)
+        y = clamp(0, round((y - GRID_PAD) / GRID_SPACING),
+                  self.canvas.height_points)
         handler(x, y)
 
     def handle_mouse_down(self, _, e, x, y):
@@ -123,7 +135,8 @@ class Workspace(TKBase):
         self.selected_tool.handle_canvas_exited()
 
     def add_line(self, x, y, dx, dy):
-        return self.canvas.add_line(x * GRID_SPACING, y * GRID_SPACING,
+        return self.canvas.add_line(x * GRID_SPACING + GRID_PAD,
+                                    y * GRID_SPACING + GRID_PAD,
                                     dx * GRID_SPACING, dy * GRID_SPACING)
 
     def delete_line(self, l):
@@ -131,5 +144,5 @@ class Workspace(TKBase):
 
     @staticmethod
     def move_line(l, x, y, dx, dy):
-        l.move_line(x * GRID_SPACING, y * GRID_SPACING,
+        l.move_line(x * GRID_SPACING + GRID_PAD, y * GRID_SPACING + GRID_PAD,
                     dx * GRID_SPACING, dy * GRID_SPACING)
