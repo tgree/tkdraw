838ee10926559ccbf14bff7af66b8483e9ccc886
Terry Greeniaus <terrygreeniaus@gmail.com>
2022-04-29 03:29:57 -0700

    tools/selection_tool.py: Update to support rectangular selection and dragging.

--- tkdraw/tools/selection_tool.py
+++ tkdraw/tools/selection_tool.py
@@ -1,32 +1,63 @@
+from enum import Enum
 
 from .tool import Tool
+from .. import geom
+from .. import coords
 from .. import icons
 
 
+class State(Enum):
+    IDLE         = 0
+    DRAG_STARTED = 1     # Clicked on an elem and dragging
+    RECT_STARTED = 2     # Clicked in open space and doing selection rect
 
 
 class SelectionTool(Tool):
     def __init__(self, workspace, x, y, w, h, *args, **kwargs):
         super().__init__(workspace, x, y, w, h, *args, **kwargs)
+        self.state            = State.IDLE
         self.nearest_elem     = None
         self.nearest_points   = []
         self.selected_elems   = set()
         self.selected_points  = []
         self.last_mouse_point = None
+        self.drag_p0          = None
+        self.drag_p1          = None
+
+        self.select_rect        = None
+        self.select_rect_elem   = None
+        self.select_rect_elems  = set()
+        self.select_rect_points = []
 
         workspace.tool_canvas.add_poly(icons.arrow.get_vertices(x, y, w, h),
                                        fill='black')
 
-    def _add_selected_points(self):
-        for se in self.selected_elems:
+    def _add_handle_points(self, elems_set, points_list):
+        for se in elems_set:
             for h in se.handles:
-                self.selected_points.append(self.workspace.add_rectangle(
+                points_list.append(self.workspace.add_rectangle(
                     h[0], h[1], -2, -2, 4, 4, fill='black'))
 
-    def _remove_selected_points(self):
-        for p in self.selected_points:
+    def _remove_handle_points(self, points_list):
+        for p in points_list:
             self.workspace.delete_canvas_elem(p)
-        self.selected_points = []
+        points_list.clear()
+
+    def _add_selected_points(self):
+        self._add_handle_points(self.selected_elems, self.selected_points)
+
+    def _remove_selected_points(self):
+        self._remove_handle_points(self.selected_points)
+
+    def _update_selected_points(self):
+        self._remove_selected_points()
+        self._add_selected_points()
+
+    def _add_select_rect_points(self):
+        self._add_handle_points(self.select_rect_elems, self.select_rect_points)
+
+    def _remove_select_rect_points(self):
+        self._remove_handle_points(self.select_rect_points)
 
     def _add_nearest_points(self, p):
         if not self.workspace.elems:
@@ -36,7 +67,7 @@ class SelectionTool(Tool):
         for e in self.workspace.elems:
             px, py = e.nearest_point(p.ex, p.ey)
             d_2    = (p.ex - px)**2 + (p.ey - py)**2
-            if not nearest or d_2 < nearest[0]:
+            if not nearest or d_2 < nearest[0]:  # pylint: disable=E1136
                 nearest = (d_2, px, py, e)
 
         if nearest[0] >= 4:
@@ -54,30 +85,70 @@ class SelectionTool(Tool):
                 h[0], h[1], -3, -3, 6, 6))
 
     def _remove_nearest_points(self):
-        for p in self.nearest_points:
-            self.workspace.delete_canvas_elem(p)
-        self.nearest_points = []
+        self._remove_handle_points(self.nearest_points)
 
-    def _go_idle(self):
-        self._remove_nearest_points()
-        self.nearest_elem  = None
+    def _translate_elems(self, elems, dx, dy):
+        if dx == 0 and dy == 0:
+            return
 
-        self._remove_selected_points()
-        self.selected_elems.clear()
+        for e in elems:
+            e.nudge(dx, dy)
 
-        self.last_mouse_point = None
+        self._update_selected_points()
+        if self.nearest_elem:
+            self._remove_nearest_points()
+            self.nearest_elem = None
+        if self.last_mouse_point:
+            self._add_nearest_points(self.last_mouse_point)
+
+    def _start_drag(self, p):
+        self.state   = State.DRAG_STARTED
+        self.drag_p0 = p
+        self.drag_p1 = p
+
+    def _start_selection_rect(self, p):
+        assert self.state == State.IDLE
+        assert not self.select_rect_elems
+        assert not self.select_rect_points
+
+        self.state            = State.RECT_STARTED
+        self.select_rect      = geom.Rect(geom.Vec(p.x, p.y),
+                                          geom.Vec(p.x, p.y))
+        self.select_rect_elem = self.workspace.add_rectangle(
+                p.x, p.y, 0, 0, 0, 0, outline='gray')
+
+    def _stop_selection_rect(self):
+        assert self.state == State.RECT_STARTED
+        self._remove_select_rect_points()
+        self.workspace.delete_canvas_elem(self.select_rect_elem)
+        self.select_rect_elems.clear()
+        self.select_rect      = None
+        self.select_rect_elem = None
+        self.state            = State.IDLE
 
     def handle_app_activated(self):
         pass
 
     def handle_app_deactivated(self):
-        pass
+        if self.state == State.RECT_STARTED:
+            self._remove_select_rect_points()
+            self.select_rect_elems.clear()
+            self.workspace.delete_canvas_elem(self.select_rect_elem)
+            self.select_rect      = None
+            self.select_rect_elem = None
+            self.state            = State.IDLE
 
     def handle_tool_selected(self):
         self.icon_border.configure(outline='black')
 
     def handle_tool_deselected(self):
-        self._go_idle()
+        assert self.state == State.IDLE
+
+        self._remove_nearest_points()
+        self.nearest_elem     = None
+        self.last_mouse_point = None
+        self._remove_selected_points()
+        self.selected_elems.clear()
         self.icon_border.configure(outline='#CCCCCC')
 
     def handle_canvas_entered(self, p):
@@ -89,9 +160,6 @@ class SelectionTool(Tool):
         self.last_mouse_point = None
 
     def handle_key_pressed(self, e):
-        if not self.selected_elems:
-            return
-
         if e.keysym in ('Left', 'Right', 'Up', 'Down'):
             if e.keysym == 'Left':
                 dx, dy = -1, 0
@@ -101,45 +169,79 @@ class SelectionTool(Tool):
                 dx, dy = 0, -1
             elif e.keysym == 'Down':
                 dx, dy = 0, 1
-
-            for se in self.selected_elems:
-                se.nudge(dx, dy)
-
-            self._remove_selected_points()
-            self._add_selected_points()
-            if self.nearest_elem:
-                self._remove_nearest_points()
-                self.nearest_elem = None
-            if self.last_mouse_point:
-                self._add_nearest_points(self.last_mouse_point)
+            self._translate_elems(self.selected_elems, dx, dy)
         elif e.keysym == 'Escape':
+            self.handle_esc_pressed()
+
+    def handle_esc_pressed(self):
+        if self.state == State.IDLE:
             self._remove_selected_points()
             self.selected_elems.clear()
+        elif self.state == State.DRAG_STARTED:
+            dx = self.drag_p0.x - self.drag_p1.x
+            dy = self.drag_p0.y - self.drag_p1.y
+            self._translate_elems(self.selected_elems, dx, dy)
+            self.drag_p0 = None
+            self.drag_p1 = None
+            self.state   = State.IDLE
+        elif self.state == State.RECT_STARTED:
+            self._stop_selection_rect()
 
     def handle_mouse_down(self, p):
+        assert self.state == State.IDLE
+
         shift_click = (p.modifiers & 1)
         if shift_click:
             if not self.nearest_elem:
-                return
+                self._start_selection_rect(p)
             elif self.nearest_elem in self.selected_elems:
                 self.selected_elems.discard(self.nearest_elem)
             else:
                 self.selected_elems.add(self.nearest_elem)
+                self._start_drag(p)
         else:
             if not self.nearest_elem:
                 self.selected_elems.clear()
+                self._start_selection_rect(p)
             elif self.nearest_elem not in self.selected_elems:
                 self.selected_elems.clear()
                 self.selected_elems.add(self.nearest_elem)
+                self._start_drag(p)
             else:
-                return
+                self._start_drag(p)
 
-        self._remove_selected_points()
-        self._add_selected_points()
+        self._update_selected_points()
 
     def handle_mouse_up(self, p):
-        pass
+        if self.state == State.DRAG_STARTED:
+            self.state = State.IDLE
+        elif self.state == State.RECT_STARTED:
+            self.selected_elems.update(self.select_rect_elems)
+            self._update_selected_points()
+            self._stop_selection_rect()
 
     def handle_mouse_moved(self, p):
         self.last_mouse_point = p
-        self._add_nearest_points(p)
+        if self.state == State.IDLE:
+            self._add_nearest_points(p)
+        elif self.state == State.DRAG_STARTED:
+            dx = p.x - self.drag_p1.x
+            dy = p.y - self.drag_p1.y
+            self.drag_p1 = p
+            self._translate_elems(self.selected_elems, dx, dy)
+        elif self.state == State.RECT_STARTED:
+            self.select_rect = geom.Rect(self.select_rect.p0,
+                                         geom.Vec(p.x, p.y))
+            self.workspace.delete_canvas_elem(self.select_rect_elem)
+            self.select_rect_elem = self.workspace.add_rectangle(
+                    self.select_rect.p0.x, self.select_rect.p0.y, 0, 0,
+                    coords.grid_to_canvas_delta(p.x - self.select_rect.p0.x),
+                    coords.grid_to_canvas_delta(p.y - self.select_rect.p0.y),
+                    outline='gray')
+
+            self._remove_select_rect_points()
+            self.select_rect_elems.clear()
+            for e in self.workspace.elems:
+                if e.overlaps_rect(self.select_rect):
+                    self.select_rect_elems.add(e)
+            self._add_select_rect_points()
