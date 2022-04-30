c0770d52ef1f70913bf7f5a937fb6c109a87859e
Terry Greeniaus <terrygreeniaus@gmail.com>
2022-04-30 01:31:19 -0700

    tkdraw/document.py: Add ability to record that handles were dragged.

--- tkdraw/document.py
+++ tkdraw/document.py
@@ -9,3 +9,7 @@ class Document:
     @staticmethod
     def elems_translated(elems, dv):
         print('%u elems translated by %s.' % (len(elems), dv))
+
+    @staticmethod
+    def elem_handle_dragged(elem, index, h0, h1):
+        print('%s handle %u dragged from %s to %s.' % (elem, index, h0, h1))
