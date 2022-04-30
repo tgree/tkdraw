cbbed4b520e57777e57e41aaf53fff0020f534d2
Terry Greeniaus <terrygreeniaus@gmail.com>
2022-04-30 01:31:19 -0700

    elems/elem.py: Add drag_handle() method.

--- tkdraw/elems/elem.py
+++ tkdraw/elems/elem.py
@@ -5,5 +5,8 @@ class Elem:
     def translate(self, dv):
         raise NotImplementedError
 
+    def drag_handle(self, index, mp):
+        raise NotImplementedError
+
     def nearest_point(self, P):
         raise NotImplementedError
