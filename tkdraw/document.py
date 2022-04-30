bf535fc84448caca48dbd691fe654fbff0157ea1
Terry Greeniaus <terrygreeniaus@gmail.com>
2022-04-30 01:31:19 -0700

    tkdraw/document.py: Add ability to delete a set of elements.

--- tkdraw/document.py
+++ tkdraw/document.py
@@ -6,6 +6,11 @@ class Document:
         print('%s added.' % elem)
         self.elems.append(elem)
 
+    def elems_delete(self, elems):
+        print('%u elems deleted.' % len(elems))
+        for e in elems:
+            self.elems.remove(e)
+
     @staticmethod
     def elems_translated(elems, dv):
         print('%u elems translated by %s.' % (len(elems), dv))
