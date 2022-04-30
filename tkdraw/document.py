b74a443667ca92cc30a5db9e8fd2d083eedd3132
Terry Greeniaus <terrygreeniaus@gmail.com>
2022-04-30 01:31:19 -0700

    tkdraw/document.py: Add ability to record that elements were translated.

--- tkdraw/document.py
+++ tkdraw/document.py
@@ -5,3 +5,7 @@ class Document:
     def elem_add(self, elem):
         print('%s added.' % elem)
         self.elems.append(elem)
+
+    @staticmethod
+    def elems_translated(elems, dv):
+        print('%u elems translated by %s.' % (len(elems), dv))
